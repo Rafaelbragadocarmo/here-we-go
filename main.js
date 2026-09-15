5d94ca1873b89c6c8581c24fc5625de2180a1755
Rafaelbragadocarmo <rafael.carmo1020@gmail.com>
2026-09-15 15:16:06 -0300

    first part

---
+++ main.js
@@ -0,0 +1,80 @@
+// let n1 = prompt("qual o seu nome?")
+// alert(`bom dia ${n1}`)
+// console.log(`bom dia ${n1}`)
+
+// let num1 = Number(prompt("qual o primeiro número?"))
+// let num2 = Number(prompt("qual o segundo número?"))
+// let som1 = num1 + num2
+// let sub1 = num1 - num2
+// let mul1 = num1 * num2
+// let div1 = num1 / num2
+// alert(`soma: ${som1}`)
+// console.log(`soma ${sub1}`)
+// alert(`subtração: ${sub1}`)
+// console.log(`subtração: ${sub1}`)
+// alert(`multiplicação: ${mul1}`)
+// console.log(`multiplicação: ${mul1}`)
+// alert(`divisão ${div1}`)
+// console.log(`divisão ${div1}`)
+
+// let ida = Number(prompt("qual a sua idade?"))
+// if (ida < 18 ) {
+//     alert("menor de idade")
+//     console.log("menor de idade")
+// }
+// else {
+//     alert("maior de idade")
+//     console.log("maior de idade")
+// }
+
+// let num3 = Number(prompt("escolha um número"))
+// if (num3 % 2 === 0) {
+//     alert("par")
+//     console.log("par")
+// }
+// else {
+//     alert("impar")
+//     console.log("impar")
+// }
+
+// let num4 = Number(prompt("escolha o primeiro número"))
+// let num5 = Number(prompt("escolha o segundo número"))
+// let num6 = Number(prompt("escolha o terceiro número"))
+// console.log(num4)
+// console.log(num5)
+// console.log(num6)
+// if (num4 < num5) {
+//     if (num5 < num6){
+// alert(`${num6} é o maior`)
+// console.log(`${num6} é o maior`)
+//     }
+//     else {
+// alert(`${num5} é o maior`)
+// console.log(`${num5} é o maior`)
+//     }
+// }
+
+// else {
+//     if (num4 < num6) {
+// alert(`${num6} é o maior`)
+// console.log(`${num6} é o maior`)
+//     }
+//     else {
+// alert(`${num4} é o maior`)
+// console.log(`${num4} é o maior`)
+//     }   
+// }
+
+// let not1 = Number(prompt("qual a sua nota"))
+// if (not1 < 4 ) {
+//     alert("reprovado")
+//     console.log("reprovado")
+// }
+// else if (not1 < 6) {
+//     alert("recuperação")
+//     console.log("recuperçã0")
+// }
+// else  {
+//     alert("aprovado")
+//     console.log("aprovado")
+// }
